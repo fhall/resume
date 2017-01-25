d827578f9bde4a7e5bc1b18a629791c0c5ec7770
Fredrik Hall <per.fredrik.hall@gmail.com>
2017-01-25 10:55:14 +0100

    Minor changes in the work positions start and end dates, added metadata (data attributes) for work positions start and end date

--- cv/work.php
+++ cv/work.php
@@ -2,31 +2,31 @@
 
   <h2>Work experience</h2>
 
-  <article>
+  <article class="position" data-position-start="2016-09" data-position-end="2016-12">
     <h3>Project Manager &amp; Evaluator</h3>
     <p><span>Sept 2016 - Dec 2016</span>, <span>Malmö university</span></p>
     <p>Project manager and evaluator of VFU-logg. The projects' goal is to develop a digital version of a log book that is used by dental technician students during internship.</p>
   </article>
 
-  <article>
+  <article class="position" data-position-start="2015-06" data-position-end="2016-12">
     <h3>Lecturer</h3>
     <p><span>June 2015 - Dec 2016</span>, <span>Malmö university</span></p>
     <p>Lecturer in computer science. Course and task design, student management, lecturing, supervision assessment etc. Among other involved in project courses, courses in information architecture, information design, web services and web development.</p>
   </article>
 
-  <article>
+  <article class="position" data-position-start="2015-06" data-position-end="2016-08">
     <h3>Project Assistant</h3>
-    <p><span>June 2015 - Sept 2016</span>, <span>Malmö university</span></p>
+    <p><span>June 2015 - Aug 2016</span>, <span>Malmö university</span></p>
     <p>Project assistant in two IoTaP research projects - DIA and Dynahmat. My contribution to DIA consisted, among other things, in developing (conceptually and prototypes) concepts for the addressing the challenges recognised in the project. My work in Dynahmat consisted mainly of developing a prototype for collecting, handling, presenting and visualising sensor data.</p>
   </article>
 
-  <article>
+  <article class="position" data-position-start="2015-01" data-position-end="2015-05">
     <h3>Teacher Assistant (Amanuensis)</h3>
-    <p><span>Jan 2015 - June 2015</span>, <span>Malmö university</span></p>
+    <p><span>Jan 2015 - May 2015</span>, <span>Malmö university</span></p>
     <p>Teacher assistant at the computer science department, Malmö university. Supervision and design of student projects, lecturing in web development and web services.</p>
   </article>
 
-  <article>
+  <article class="position" data-position-start="2014-04" data-position-end="2014-05">
     <h3>Data Enabler, Mentor and Hack Resource</h3>
     <p><span>April 2014 - May 2014</span>, <span>Traffic Jam Session, hackathon</span></p>
     <p>Collected, organised, prepared and made available data for the traffic jam session hackathon. During the hackathon I also acted as a mentor for one team and general resource for all the teams.</p>
