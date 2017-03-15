700bfa8b5d1c6d9e4a1d33433ae950a8cd784774
Fredrik Hall <per.fredrik.hall@gmail.com>
2017-03-15 22:23:11 +0100

    Changed profile image max size and changed size of #wrapper when on devices with smaller screens

--- index.php
+++ index.php
@@ -29,6 +29,7 @@
 <html>
 <head>
 	<title>fhall</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans|Slabo+27px" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
